9e61d1e76fb898584f28988308359ec2f600b0d1
Aniket <aniantre@gmail.com>
2025-01-16 20:22:55 +0530

    added implementation of three sum using two pointer

--- DSA/Two-Pointer/Three_Sum_ElementReturn.cpp
+++ DSA/Two-Pointer/Three_Sum_ElementReturn.cpp
@@ -3,15 +3,15 @@
 #include <vector>
 using namespace std;
 class ThreeSUM {
-	public:
-		vector<vector<int>> Three_Sum (vector<int>&nums, int target){
-			// check if array size  is less than 3
-			if (nums.size()<3) return {};
-			//to use three pointer, we need to sort the array
-			sort(nums.begin(), nums.end());
+  public:
+    vector<vector<int>> Three_Sum (vector<int>&nums, int target){
+      // check if array size  is less than 3
+      if (nums.size()<3) return {};
+      //to use three pointer, we need to sort the array
+      sort(nums.begin(), nums.end());
             vector<vector<int>> result;
-			//always put left pointer on 1st index and right on last index
-			for (int fixed_Pointer = 0; fixed_Pointer<nums.size()-2; fixed_Pointer++){
+      //always put left pointer on 1st index and right on last index
+      for (int fixed_Pointer = 0; fixed_Pointer<nums.size()-2; fixed_Pointer++){
         if (fixed_Pointer>0 && nums[fixed_Pointer] == nums[fixed_Pointer-1]) continue;
         int left = fixed_Pointer+1;
         int right = nums.size()-1;
@@ -34,27 +34,27 @@ class ThreeSUM {
           }
         }
       }
-			return result;
-		}
+      return result;
+    }
 };
 
 int main(){
-	vector<int> vec = {2, 4, -6, 8, 5, 6, 3, 9};
-	int target_sum = 0;
-	//bool ans;
+  vector<int> vec = {2, 4, -6, 8, 5, 6, 3, 9};
+  int target_sum = 0;
+  //bool ans;
     vector<vector<int>> ans;
-	ThreeSUM check_target;
-	ans = check_target.Three_Sum(vec, target_sum);
+  ThreeSUM check_target;
+  ans = check_target.Three_Sum(vec, target_sum);
     for (int i = 0; i<ans.size(); i++){
         for (int j = 0; j<ans[i].size(); j++){
           cout<<ans[i][j]<<" ";
         }
         cout<<endl;
     }
-	// if (ans){
-	// 	cout<<"True"<<endl;
-	// }
-	// else{
-	// 	cout<<"False"<<endl;
-	// }
+  // if (ans){
+  // 	cout<<"True"<<endl;
+  // }
+  // else{
+  // 	cout<<"False"<<endl;
+  // }
 }
